256ebac7e3861d75387e122f9d1e130673384079
oahmed105 <oahmed10598@gmail.com>
2023-07-22 19:15:18 -0400

    Classroom classs mostly complete

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,63 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Classroom {
+
+    private Student[] pupils;
+
+    public Classroom(Student[] pupils) {
+        this.pupils = pupils;
+    }
+
+    public Classroom(int maxNumberOfStudents) {
+        this.pupils = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom() {
+        this.pupils = new Student[30];
+    }
+
+    public Student[] getPupils() {
+        return pupils;
+    }
+
+    public Double getAverageExamScore() {
+        Double sum = 0.0;
+        for (Student student: pupils) {
+            sum += student.getAverageExamScore();
+        }
+        return sum / pupils.length;
+    }
+
+    public void addStudent(Student student) {
+        ArrayList<Student> temp = new ArrayList<>(Arrays.asList(pupils));
+        temp.add(student);
+        Student [] updatedArr = temp.toArray(new Student[0]);
+        this.pupils = updatedArr;
+    }
+
+    public void removeStudent(String firstName, String lastName) {
+        ArrayList<Student> temp = new ArrayList<>(Arrays.asList(pupils));
+        for (Student student: temp) {
+            if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName)) {
+                temp.remove(student);
+            }
+        }
+        temp.add(null);
+        Student [] updatedArr = temp.toArray(new Student[0]);
+        this.pupils = updatedArr;
+    }
+
+//    public Student[] getStudentByScore() {
+//        ArrayList<Student> temp = new ArrayList<>(Arrays.asList(pupils));
+//        for (Student student: pupils) {
+//            student.getAverageExamScore();
+//            do {
+//
+//            } while (student.getAverageExamScore())
+//        }
+//    }
+
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,40 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class ClassroomTest {
+
+    @Test
+    public void averagetest() {
+        // : Given
+        // : Given
+//        int maxNumberOfStudents = 3;
+//        Classroom classroom = new Classroom(3);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student("Leon", "Hunter", examScores);
+        Student student1 = new Student("John", "Hunter", examScores);
+        Student student2 = new Student("Leon", "Hunter", examScores);
+        Student[] students = new Student[]{student, student1, student2};
+        Classroom classroom = new Classroom(students);
+
+        // When
+//        classroom.addStudent(student);
+//        classroom.addStudent(student1);
+//        classroom.addStudent(student2);
+        Student[] preEnrollment = classroom.getPupils();
+
+        classroom.removeStudent("John", "Hunter");
+        Student[] postEnrollment = classroom.getPupils();
+
+        // Then
+        String preEnrollmentAsString = Arrays.toString(preEnrollment);
+        String postEnrollmentAsString = Arrays.toString(postEnrollment);
+
+        System.out.println("===========================");
+        System.out.println(preEnrollmentAsString);
+        System.out.println("===========================");
+        System.out.println(postEnrollmentAsString);
+    }
 }
