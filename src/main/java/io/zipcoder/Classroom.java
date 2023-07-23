e40b4049ebddd822bc3f3da5f335391a24f7e079
oahmed105 <oahmed10598@gmail.com>
2023-07-23 12:27:59 -0400

    completed one more method

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -50,14 +50,31 @@ public class Classroom {
         this.pupils = updatedArr;
     }
 
-//    public Student[] getStudentByScore() {
-//        ArrayList<Student> temp = new ArrayList<>(Arrays.asList(pupils));
-//        for (Student student: pupils) {
-//            student.getAverageExamScore();
-//            do {
-//
-//            } while (student.getAverageExamScore())
-//        }
-//    }
+    public Student[] getStudentsByScore() {
+        Student[] sortedStudents = Arrays.copyOf(pupils, pupils.length);
+        int n = sortedStudents.length;
+
+        // Bubble sort in descending order of score
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (sortedStudents[j].getAverageExamScore() < sortedStudents[j + 1].getAverageExamScore()) {
+                    // Swap the students if the j-th student has a lower score than (j+1)-th student
+                    Student temp = sortedStudents[j];
+                    sortedStudents[j] = sortedStudents[j + 1];
+                    sortedStudents[j + 1] = temp;
+                } else if (sortedStudents[j].getAverageExamScore() == sortedStudents[j + 1].getAverageExamScore()) {
+                    // If the scores are equal, compare the names lexigraphically
+                    if (sortedStudents[j].getLastName().compareTo(sortedStudents[j + 1].getLastName()) > 0) {
+                        // Swap the students if the names are in lexicographic order (i.e., the j-th student's name comes after (j+1)-th student's name)
+                        Student temp = sortedStudents[j];
+                        sortedStudents[j] = sortedStudents[j + 1];
+                        sortedStudents[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        return sortedStudents;
+    }
 
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -12,29 +12,53 @@ public class ClassroomTest {
         // : Given
 //        int maxNumberOfStudents = 3;
 //        Classroom classroom = new Classroom(3);
-        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
-        Student student = new Student("Leon", "Hunter", examScores);
-        Student student1 = new Student("John", "Hunter", examScores);
-        Student student2 = new Student("Leon", "Hunter", examScores);
-        Student[] students = new Student[]{student, student1, student2};
-        Classroom classroom = new Classroom(students);
+        Double[] examScores = { 100.0, 90.0, 95.0, 60.0 };
+        Double[] examScores1 = { 100.0, 85.0, 50.0, 0.0 };
+        Double[] examScores2 = { 100.0, 90.0, 95.0, 60.0 };
+//        Student student = new Student("Leon", "Hunter", examScores);
+//        Student student1 = new Student("John", "Hunter", examScores1);
+//        Student student2 = new Student("Leon", "Hunter", examScores2);
+//        Student[] students = new Student[]{student, student1, student2};
+//        Classroom classroom = new Classroom(students);
+//
+//        // When
+////        classroom.addStudent(student);
+////        classroom.addStudent(student1);
+////        classroom.addStudent(student2);
+//        Student[] preEnrollment = classroom.getPupils();
+////        classroom.getAverageExamScore();
+//        String studentScores = classroom.getStudentByScore().toString();
+//
+////        classroom.removeStudent("John", "Hunter");
+//        Student[] postEnrollment = classroom.getPupils();
+//
+//        // Then
+//        String preEnrollmentAsString = Arrays.toString(preEnrollment);
+//        String postEnrollmentAsString = Arrays.toString(postEnrollment);
+//
+////        System.out.println("===========================");
+////        System.out.println(preEnrollmentAsString);
+////        System.out.println("===========================");
+////        System.out.println(postEnrollmentAsString);
+//        System.out.println(studentScores);
 
-        // When
-//        classroom.addStudent(student);
-//        classroom.addStudent(student1);
-//        classroom.addStudent(student2);
-        Student[] preEnrollment = classroom.getPupils();
 
-        classroom.removeStudent("John", "Hunter");
-        Student[] postEnrollment = classroom.getPupils();
+            // Create some Student objects
+//            Student student1 = new Student("John", "Doe", examScores );
+            Student student2 = new Student("Alice","Johnson", examScores);
+            Student student3 = new Student("Bob","Smith", examScores1);
+//            Student student4 = new Student("Alice Adams", 90);
+            Student student5 = new Student("Jane","Smith", examScores2);
 
-        // Then
-        String preEnrollmentAsString = Arrays.toString(preEnrollment);
-        String postEnrollmentAsString = Arrays.toString(postEnrollment);
+            // Create a Classroom and add the students
+            Classroom classroom = new Classroom();
+//            classroom.addStudent(student1);
+            classroom.addStudent(student2);
+            classroom.addStudent(student3);
+//            classroom.addStudent(student4);
+            classroom.addStudent(student5);
 
-        System.out.println("===========================");
-        System.out.println(preEnrollmentAsString);
-        System.out.println("===========================");
-        System.out.println(postEnrollmentAsString);
+            // Get the sorted students
+            Student[] sortedStudents = classroom.getStudentsByScore();
     }
 }
